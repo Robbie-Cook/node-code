6b40edd818ff9fd61c84b809e12b50ee505beedc
Robbie Cook <robbieck7@gmail.com>
2020-09-03 22:11:25 +1200

    Fixing interface behaviour

--- app/components/Interface.tsx
+++ app/components/Interface.tsx
@@ -11,11 +11,31 @@ interface InterfaceProps {
   children?: any;
 }
 
+let consoleReference = console.log;
+
 /**
   A Interface component.
 */
 const Interface: React.FC<InterfaceProps> = (props) => {
-  const [text, setText] = React.useState('Enter text here');
+  const [text, setText] = React.useState(`console.log('hello')`);
+  const [output, setOutput] = React.useState<string>();
+  
+  let func: Function | null = null;
+  try {
+    func = new Function(text);
+  } catch (e) {
+    // setOutput(e.toString());
+  }
+
+  React.useEffect(() => {
+    console.log = (output: string) => setOutput(output);
+
+    func?.();
+
+    () => (console.log = consoleReference);
+  }, []);
+
+
   return (
     <section
       css={css`
@@ -28,14 +48,24 @@ const Interface: React.FC<InterfaceProps> = (props) => {
           width: 600px;
         `}
       >
-        <Editor onChange={setText} value={text} />
+        <Editor
+          onChange={(text) => {
+            setText(text);
+            try {
+              func?.();
+            } catch (e) {
+              setOutput(e.toString());
+            }
+          }}
+          value={text}
+        />
       </Pane>
       <Pane
         css={css`
           width: 50%;
         `}
       >
-        {text}
+        {output}
       </Pane>
     </section>
   );
